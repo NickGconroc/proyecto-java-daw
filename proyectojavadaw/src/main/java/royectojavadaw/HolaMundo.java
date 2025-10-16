31197d81ba63956e073385a47406dce57fa7b112
gino <gconroc550@g.educaand.es>
2025-10-16 13:17:07 +0200

    Añadir clase principal HolaMundo.java

---
+++ proyectojavadaw/src/main/java/royectojavadaw/HolaMundo.java
@@ -0,0 +1,7 @@
+package royectojavadaw;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("¡Hola, Mundo! Este es mi primer programa versionado con Git");
+    }
+}
