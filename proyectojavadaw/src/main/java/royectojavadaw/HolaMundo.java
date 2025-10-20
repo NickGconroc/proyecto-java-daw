b8776906573e13faaa08e04a28008b12986281cc
gino <gconroc550@g.educaand.es>
2025-10-20 09:31:04 +0200

    CAmbio archivo en rama origin/holamundo

--- proyectojavadaw/src/main/java/royectojavadaw/HolaMundo.java
+++ proyectojavadaw/src/main/java/royectojavadaw/HolaMundo.java
@@ -3,5 +3,6 @@ package royectojavadaw;
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("¡Hola, Mundo! Este es mi primer programa versionado con Git");
+        System.out.println("probando estoy en otra rama originn/holamundo");
     }
 }
